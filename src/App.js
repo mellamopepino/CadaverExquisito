fb2a1bbc7d91044ea60bb6873d62f76155bc4a29
mellamopepino <delfinagarciav@gmail.com>
2020-05-15 17:55:02 -0300

    First contribution
    A loading parrot, just because.
    What is loading? Nothing really, I just like that parrot.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (<></>);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false)
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [])
+
+    return (
+        <div>
+            {loading &&
+                <div class="loading-app">
+                    <img src="https://i.giphy.com/gFhSNeQ1Tca65woJbc.gif"/>
+                    <p>Loading parrot...</p>
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
